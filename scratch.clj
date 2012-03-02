9930cd39aea089b89d21d39f8ec4801f296f8b47
Craig Andera <candera@wangdera.com>
2012-03-02 17:03:50 -0500

    [WIP] Started figuring out how to define interop types

--- scratch.clj
+++ scratch.clj
@@ -41,8 +41,65 @@ MethodAttributes/PinvokeImpl
 
 (message-box nil "foo" "bar" (uint 6))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Objective: pass a structure to an APIcall GetCursorPos.
+;; Demonstrates marshalling structures and ref arguments.
+
+;; BOOL WINAPI GetCursorPos(
+;;   __out  LPPOINT lpPoint
+;; );
+;;
+;; typedef struct tagPOINT {
+;;   LONG x;
+;;   LONG y;
+;; } POINT, *PPOINT;
+
+(import 'System.Reflection.FieldAttributes)
+
+(defn interop-type [spec]
+  (let [assembly-name (str (gensym "assembly"))
+        assembly-builder (.DefineDynamicAssembly AppDomain/CurrentDomain
+                                                 (AssemblyName. assembly-name)
+                                                 AssemblyBuilderAccess/Run)
+        module-builder (.DefineDynamicModule assembly-builder "interop")
+        type-builder (.DefineType module-builder
+                                  "InteropModule"
+                                  (enum-or TypeAttributes/Public
+                                           TypeAttributes/UnicodeClass))]
+    (doseq [[name type] (partition 2 spec)]
+      (.DefineField type-builder
+                    name
+                    type
+                    FieldAttributes/Public))
+    (.CreateType type-builder)))
+
+;; (def tbld  (interop-type ["x" Int32 "y" Int32]))
+;; (.DefineField tbld "x" Int32 (into-array FieldAttributes [FieldAttributes/Public]))
+
+(def point-type (interop-type ["x" Int32 "y" Int32]))
+
+(def point (Activator/CreateInstance point-type))
+
+(.x point)
+
+(set! (. point x) (int 3))              ; Doesn't work - type mismatch
+
+(.x point)
+(.x (Activator/CreateInstance point-type))
+
+(s)
 
 
 
+(System.Reflection.Assembly/LoadWithPartialName "System.Drawing")
+(import 'System.Drawing.Point)
 
+(Point. 12 15)
+
+(. (Point. 12 14) X)
+
+(def get-cursor-pos (pinvoke "user32.dll" "GetCursorPos"
+                             Boolean
+                             []))
 
